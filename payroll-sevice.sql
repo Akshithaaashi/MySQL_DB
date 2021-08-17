be61b29f68a2ae3e05c42e200e90135f60d5a501
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-17 23:31:47 +0530

    Added : employee payroll table

--- payroll-sevice.sql
+++ payroll-sevice.sql
@@ -2,3 +2,12 @@
 create database payroll_services_DB;
 show databases;
 use payroll_services_DB;
+#uc2
+create table emp_payroll(
+	id int unsigned not null auto_increment,
+    name varchar(150) not null,
+    salary double not null,
+    start date not null,
+    primary key (id)
+);
+select * from emp_payroll;
