1b629771641b707828844ef1b883632139d2b594
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-17 22:52:28 +0530

    Added : emplyee payroll table

--- sql DB.sql
+++ sql DB.sql
@@ -1,3 +1,13 @@
 #uc1
 show databases;
 use payroll_services;
+
+#uc2
+create table emp_payroll(
+	id int unsigned not null auto_increment,
+    name varchar(150) not null,
+    salary double not null,
+    start date not null,
+    primary key (id)
+);
+select * from emp_payroll;
