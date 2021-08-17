4e6e42ed91419aacb30f55706744ecaf7878e645
Akshithaaashi <akshitha.aashi@gmail.com>
2021-08-17 23:37:41 +0530

    Added : employee payroll data

--- payroll-sevice.sql
+++ payroll-sevice.sql
@@ -11,3 +11,12 @@ create table emp_payroll(
     primary key (id)
 );
 select * from emp_payroll;
+
+#uc3
+insert into emp_payroll (name,salary, start) values
+('Akshitha',4500.0,'2021-02-16'),
+('Aishu',5500.0,'2021-04-02'),
+('Arjun',7500.0,'2021-05-20'),
+('Shubam',8750.0,'2021-01-17'),
+('Sankar',6280.0,'2021-03-07'),
+('SAHIL',2540.0,'2021-06-08');
